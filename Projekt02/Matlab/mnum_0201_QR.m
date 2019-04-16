c7367cf68ebbfb7401d90dd536289ba660a3c03c
lswitaj <lswitaj@mion.elka.pw.edu.pl>
2019-04-16 07:22:14 +0200

    added function which factors also non-square matrixes

--- Projekt02/Matlab/mnum_0201_QR.m
+++ Projekt02/Matlab/mnum_0201_QR.m
@@ -9,11 +9,15 @@ clear;
 %         macierz_niesymetryczna(rozmiar);
 %     end
 % end
-A = [1 1 9;2 -1 0; -2 4 0];
+%A = [1 1 9;2 -1 0; -2 4 0];
+%A = [1 1;2 -1; -2 4];
+%A = [1 1 2; -1 -2 4];
 A = macierz_symetryczna(5);
-[Q R] = qr_rozklad(A)
-eig(A)
-qr_bezprzesuniec(A)
+    
+[q r] = qr_rozklad_niekwadrat(A)
+[Q R] = qr(A)
+%eig(A)
+%qr_bezprzesuniec(A)
 %% 
 % wyswietlenie bledow
 
@@ -28,6 +32,7 @@ function A = qr_bezprzesuniec(A)
         [Q R] = qr_rozklad(A);
         A = R * Q;
     end
+    A = wektor(A);
 end
 %% 
 % algorytm obliczania wartosci wlasnych metoda QR z przesunieciami
@@ -35,8 +40,41 @@ end
 function qrp = qr_przesuniecia(A)
     qrp = 0;
 end
+%% rozklad QR dla macierzy niekwadratowych
+
+function [Q R] = qr_rozklad_niekwadrat(A)
+     [r_wiersze r_kolumny] = size(A);
+     Q = zeros(r_wiersze);
+     if r_wiersze > r_kolumny
+        R = eye(r_wiersze);
+     else
+        R = eye(r_kolumny);
+     end
+    %Gram-Schmidt
+     for i = 1:r_kolumny
+         Q(:,i) = A(:,i);
+         for j = 1:(i-1)
+             R(j,i) = mydot(Q(:,j),A(:,i))/mydot(Q(:,j),Q(:,j));
+             Q(:,i) = Q(:,i) - R(j,i)*Q(:,j);
+         end
+     end
+     Q = Q(1:r_wiersze,1:r_kolumny);
+     %normalizacja
+     N = zeros(r_wiersze);
+     for i = 1:r_kolumny
+         N(i,i) = norm(Q(:,i));
+         Q(:,i) = Q(:,i)/N(i,i);
+     end
+     R = N*R;
+     
+     if r_wiersze > r_kolumny
+        R = R(1:r_kolumny,1:r_kolumny);
+     else
+        R = R(1:r_wiersze,1:r_wiersze);
+     end
+end
 %% 
-% rozklad QR
+% rozklad QR dla macierzy kwadratowych
 
 function [Q R] = qr_rozklad(A)
      rozmiar = size(A);
@@ -69,18 +107,27 @@ function md = mydot(A,B)
     end
 end
 %% 
+% funkcja wektoryzujaca macierz diagonalna
+
+function w = wektor(A)
+    rozmiar = size(A);
+    for i = 1:rozmiar
+        w(i,1) = A(i,i);
+    end
+end
+%% 
 % sprawdzenie tolerancji
 
 function tol = tolerancja(A)
     rozmiar = size(A);
-    A = abs(A)
+    A = abs(A);
     tol = 0;
     for i = 1:rozmiar
         if max(A(i,i+1:end)) > tol
-            tol = max(A(i,i+1:end))
+            tol = max(A(i,i+1:end));
         end
         if max(A(i,1:i-1)) > tol
-            tol = max(A(i,1:i-1))
+            tol = max(A(i,1:i-1));
         end
     end
 end
