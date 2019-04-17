0498e4494bd1d87b22c7d1faa77a02ee7bbe060b
lswitaj <lswitaj@mion.elka.pw.edu.pl>
2019-04-17 13:24:25 +0200

    added qr with shifts

--- Projekt02/Matlab/mnum_0201_QR.m
+++ Projekt02/Matlab/mnum_0201_QR.m
@@ -11,12 +11,13 @@ clear;
 % end
 A = [1 1;2 -1; -2 4];
 %A = [1 1 2; -1 -2 4];
-A = macierz_symetryczna(20);
+A = macierz_symetryczna(4);
 A
 % [q r] = qr_rozklad(A)
 % [Q R] = qr(A)
 eig(A)
 [A i] = qr_bezprzesuniec(A)
+[A i] = qr_przesuniecia(A)
 %% 
 % wyswietlenie bledow
 
@@ -63,25 +64,49 @@ end
 %% 
 % algorytm obliczania wartosci wlasnych metoda QR bez przesuniec
 
-function [A i] = qr_bezprzesuniec(A)
+function [wart_wlasne i] = qr_bezprzesuniec(A)
     i = 0;
-    while tolerancja(A) > 0.00001 & i < 1000
+    while tolerancja(A) > 0.00001 & i < 1000+1
         [Q R] = qr_rozklad(A);
         A = R * Q;
         i = i+1;
     end
-    A = wektor(A);
+    wart_wlasne = wektor(A);
 end
 %% 
 %  algorytm obliczania wartosci wlasnych metoda QR z przesunieciami
 
-function qrp = qr_przesuniecia(A)
-    qrp = 0;
+function [wart_wlasne i] = qr_przesuniecia(A)
+    rozmiar = size(A);
+    i = 0;
+    wart_wlasne = zeros(rozmiar);
+    wart_wlasne = wart_wlasne(:,1);
+    for j = rozmiar:-1:2
+        while tolerancja(A) > 0.00001 & i < 1000+1
+            mala_macierz = A(k-1:k,k-1:k);                                    % macierz 2x2,
+            przesuniecie = blizsza_liczba(pierw_f_kwadratowej(mala_macierz)); % z ktorej wyznaczana jest najlepsza wart. wlasna
+            A = A - eye(j)*przesuniecie;
+            [Q R] = qr_rozklad(A);
+            A = R * Q + eye(j)*przesuniecie;
+            i = i+1;
+        end
+        wart_wlasne(j) = A(j,j);
+        if j > 2
+            A = A(1:j-1,1:j-1);             %deflacja
+        else
+            wart_wlasne(j) = A(1,1);
+        end
+    end
+    wart_wlasne;
 end
 %% 
-% wyznaczanie wiekszego pierw f. kwadratowej
+% wyznaczanie pierw f. kwadratowej
 
-function [x1 x2] = pierw_f_kwadratowej(a,b,c)
+function [x1 x2] = pierw_f_kwadratowej(mala_macierz)
+    a = 1;
+    b = -(mala_macierz(1,1)+mala_macierz(2,2));
+    c = (mala_macierz(1,1)*mala_macierz(2,2))-(mala_macierz(2,1)*mala_macierz(1,2));
+    
     x1 = (-b + sqrt(b*b - 4*a*c))/(2*a);
     x2 = (-b - sqrt(b*b - 4*a*c))/(2*a);
     if abs(x2) > abs(x1)
@@ -89,6 +114,16 @@ function [x1 x2] = pierw_f_kwadratowej(a,b,c)
     end
    %drugi pierwiastek ze wzorów Viete'a
     x2 = ((-b)/a) - x1;
+end
+%% 
+% wybor pierwiastka blizszego d(n,n)
+
+function x = blizsza_liczba(wlasciwa, x1, x2)
+    if abs(wlasciwa-x1) < abs(wlasciwa-x1)
+        x = x1;
+    else
+        x = x2;
+    end
 end
 %% 
 % autorska implementacja matlabowej funkcji dot()
@@ -116,13 +151,19 @@ function tol = tolerancja(A)
     rozmiar = size(A);
     A = abs(A);
     tol = 0;
-    for i = 1:rozmiar
-        if max(A(i,i+1:end)) > tol
-            tol = max(A(i,i+1:end));
+    if rozmiar > 2
+        for i = 1:rozmiar
+            if max(A(i,i+1:end)) > tol
+                tol = max(A(i,i+1:end));
+            end
+            if max(A(i,1:i-1)) > tol
+                tol = max(A(i,1:i-1));
+            end
         end
-        if max(A(i,1:i-1)) > tol
-            tol = max(A(i,1:i-1));
-        end
+    elseif rozmiar == 2
+        tol = max([A(1,2) A(2,1)]);
+    else
+        tol = 0;
     end
 end
 %% 
