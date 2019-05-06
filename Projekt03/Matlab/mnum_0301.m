cade9c49f651dce4e9c8193e4b0658195e542482
lswitaj <lswitaj@mion.elka.pw.edu.pl>
2019-05-06 17:18:40 +0200

    all methods work

--- Projekt03/Matlab/mnum_0301.m
+++ Projekt03/Matlab/mnum_0301.m
@@ -4,9 +4,6 @@
 %   [wart indx] = max(cond_sym(:,rozmiar));
 %   iteracje_sym_bezprzes(indx,rozmiar) = 0;
 
-% warunek stopu
-%   while tol(fun(x)) > 0.00001 & i < 200+1
-
 % % wyznaczanie pierw f. kwadratowej
 % function [x1 x2] = pierw_f_kwadratowej(mala_macierz)
 %     a = 1;
@@ -22,18 +19,29 @@
 %     x2 = ((-b)/a) - x1;
 % end
 %% 
-% 
-% 
 % start
 
 clc;
 clear;
 x = linspace(0,15,100);
 %% 
+% przedzialy poczatkowe
+
+przedzialy = [6 8; 8.3 10; 12 15];
+
+dokladnosc_zer = 0.001;
+wielkosc_przedzialu = 0.001;
+
+for i=1:3
+    %wybor i-tego przedzialu
+    sprawdzenie_przedzialu(przedzialy(i,:));
+end
+%% 
 % wykres
 
 figure
 y = wartosc_funkcji(x);
+y2 = wartosc_funkcji(przedzialy);
 plot(x,y,'b-',[0 15], [0 0], 'k--');
 legend({'f(x)', 'y=0'},'Location','southwest');
 title('wykres funkcji f(x)=2,3*sin(x)+4*ln(x+2)-11');
@@ -46,47 +54,37 @@ title('wykres funkcji f(x)=2,3*sin(x)+4*ln(x+2)-11');
 % wartosc_funkcji(9)
 % wartosc_funkcji(12.5)
 %% 
-% przedzialy poczatkowe
+% porownanie
 
-    przedzialy = [1 8; 8 10; 10 15];
-
-for i=1:3
-    %wybor i-tego przedzialu
-    sprawdzenie_przedzialu(przedzialy(i,:));
-end
-%% 
-% metoda bisekcji
-
-x_bisekcja = bisekcja(przedzialy);
-x_bisekcja
+x_bisekcja = bisekcja(przedzialy, dokladnosc_zer, wielkosc_przedzialu)
+x_sieczne = met_siecznych(przedzialy, dokladnosc_zer, wielkosc_przedzialu)
+x_styczne = met_stycznych(przedzialy, dokladnosc_zer, wielkosc_przedzialu)
 y_bisekcja = wartosc_funkcji(x_bisekcja)
+y_sieczne = wartosc_funkcji(x_sieczne)
+y_styczne = wartosc_funkcji(x_styczne)
+
 fprintf('najwiekszy blad zera przy metodzie bisekcji %f\n', najwieksze_zero(x_bisekcja));
+fprintf('najwiekszy blad zera przy metodzie siecznych %f\n', najwieksze_zero(x_sieczne));
+fprintf('najwiekszy blad zera przy metodzie stycznych %f\n', najwieksze_zero(x_styczne));
 
 figure
 plot(x,y,'b-',[0 15], [0 0], 'k--', x_bisekcja, wartosc_funkcji(x_bisekcja), 'go');
 legend({'f(x)', 'y=0', 'm. zerowe met. bisekcji'},'Location','southwest');
 title('wykres funkcji f(x)=2,3*sin(x)+4*ln(x+2)-11');
-%% 
-% metoda siecznych
 
-x_sieczne = met_siecznych(przedzialy);
-x_sieczne
-y_sieczne = wartosc_funkcji(x_sieczne)
-fprintf('najwiekszy blad zera przy metodzie siecznych %f\n', najwieksze_zero(x_sieczne));
-
-figure
 plot(x,y,'b-',[0 15], [0 0], 'k--', x_sieczne, wartosc_funkcji(x_sieczne), 'go');
 legend({'f(x)', 'y=0', 'm. zerowe met. siecznych'},'Location','southwest');
+title('wykres funkcji f(x)=2,3*sin(x)+4*ln(x+2)-11');
+
+plot(x,y,'b-',[0 15], [0 0], 'k--', x_styczne, wartosc_funkcji(x_styczne), 'go');
+legend({'f(x)', 'y=0', 'm. zerowe met. stycznych'},'Location','southwest');
 title('wykres funkcji f(x)=2,3*sin(x)+4*ln(x+2)-11');
 %% 
 % *funkcje pomocnicze glowne*
 % 
-% metoda bisekcji
+% *1. metoda bisekcji*
 
-function x = bisekcja(przedzialy)
-    dokladnosc_zer = 0.1;
-    wielkosc_przedzialu = 0.1;
-    
+function x = bisekcja(przedzialy, dokladnosc_zer, wielkosc_przedzialu)
     ilosc_pierwiastkow = size(przedzialy,1);
     x = zeros(ilosc_pierwiastkow);
     x = wektor(x);
@@ -110,12 +108,9 @@ function [c nowy_przedzial] = polowienie_przedzialu(przedzial)
     end
 end
 %% 
-% metoda siecznych
+% *2. metoda siecznych*
 
-function x = met_siecznych(przedzialy)
-    dokladnosc_zer = 0.1;
-    wielkosc_przedzialu = 0.1;
-
+function x = met_siecznych(przedzialy, dokladnosc_zer, wielkosc_przedzialu)
     ilosc_pierwiastkow = size(przedzialy,1);
     x = zeros(ilosc_pierwiastkow);
     x = wektor(x);
@@ -148,6 +143,50 @@ function c = wyznacz_zero_f_liniowej(przedzial)
     y1 = wartosc_funkcji(x1);
     y2 = wartosc_funkcji(x2);
     c = (x2*y1-x1*y2)/(y1-y2);
+end
+%% 
+% *3. metoda stycznych*
+
+function x = met_stycznych(przedzialy, dokladnosc_zer, wielkosc_przedzialu)
+    ilosc_pierwiastkow = size(przedzialy,1);
+    x = zeros(ilosc_pierwiastkow);
+    x = wektor(x);
+    for i = 1:ilosc_pierwiastkow
+        c = 0;
+        pierwotny_przedzial = przedzialy(i,:);
+        while (abs(wartosc_funkcji(c))>dokladnosc_zer)
+            [c przedzialy(i,:)] = nowy_przedzial_sieczny(przedzialy(i,:), pierwotny_przedzial);
+        end
+        x(i) = c;
+    end
+end
+%% 
+% zmniejszenie przedzialow dla metody stycznych
+
+function [d nowy_przedzial] = nowy_przedzial_sieczny(przedzial, pierwotny_przedzial)
+    d = nowy_punkt_sieczny(przedzial(1));
+    if(d >= pierwotny_przedzial(1) & d <= pierwotny_przedzial(2))
+        nowy_przedzial = [d przedzial(2)];
+    else
+        disp(przedzial);
+        disp(d);
+        error('Error. \nPunkt poza przedzialem');
+    end
+end
+%% 
+% wyznaczenie miejsca zerowego na podstawie pochodnej oraz poprzedniego 
+% punktu
+
+function x0 = nowy_punkt_sieczny(x)
+    m = wartosc_pochodnej(x);
+    y = wartosc_funkcji(x);
+    x0 = (m*x-y)/m;
+end
+%% 
+% wyznaczenie pochodnej dla podanych x-ow
+
+function y = wartosc_pochodnej(x)
+    y = 23*cos(x)/10+4/(x+2);
 end
 %% 
 % *funkcje pomocnicze dodatkowe*
